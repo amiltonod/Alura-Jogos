33973655528f5a820811e27296323a67243068af
Amilton Junior <amilton.od@gmail.com>
2022-10-26 19:27:03 -0300

    Listas

--- forca.py
+++ forca.py
@@ -4,12 +4,24 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
-
+    letras_acertadas = ['_', '_', '_', '_', '_', '_']
     enforcou = False
     acertou = False
 
+    print(letras_acertadas)
+
     while not acertou and not enforcou:
-        print("Jogando...")
+
+        chute = input('Digite a palavra: ').strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                letras_acertadas[index] = letra
+                print(f'Encontrei a letra {chute} na posição {index}')
+            index = index + 1
+
+        print(letras_acertadas)
 
     print("Fim do jogo")
 
